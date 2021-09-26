6a8d0a03761b3c6598c3650624a9ccea31a448ee
Eder Lara Trujillo <ederlatru@gmail.com>
2021-09-26 12:02:56 -0500

    2. Creación de Formulario de Perfil, Ajuste usuario = user llave foranea

--- registration/urls.py
+++ registration/urls.py
@@ -5,8 +5,9 @@
 # Importar los paquetes de admin de urls:
 from django.urls import path                        # Admin de Rutas
 from django.urls.resolvers import URLPattern        # Resuelve las Rutas
-from .views import SingUpView                       # Vista donde resuelvo la ruta
+from .views import SingUpView, ProfileUpdate        # Vista donde resuelvo la ruta
 
 urlpatterns = [ 
-    path('singup/', SingUpView.as_view(), name="singup")
+    path('singup/', SingUpView.as_view(), name="singup"),
+    path('profile/', ProfileUpdate.as_view(), name="profile"),
  ]
